e44ff797e32eacd8ed3e76ece4953b9aaff17161
Kram037 <defenzomarco@gmail.com>
2022-11-10 13:02:08 +0100

    fix: fixed graphic aspect of printGarageDetails

--- src/it/entities/Garage.java
+++ src/it/entities/Garage.java
@@ -17,7 +17,7 @@ public class Garage {
 
     /**
      * method that uses a for loop to cycle the array garageLots and check every lot to find the first empty lot 
-     * to add an element
+     * to add an element.
      * @param vehicle to insert in a parking lot
      */
     public void addVehicle(MotorVehicle vehicle) {
@@ -33,7 +33,7 @@ public class Garage {
     }
 
     /**
-     * method to remove an item from the array returning an instance of the object itself
+     * method to remove an item from the array returning an instance of the object itself.
      * @param lot number of the garage lot
      * @return removed object
      */
@@ -44,12 +44,12 @@ public class Garage {
         try {
             removedVehicle = garageLots[lot];
             if (garageLots[lot] == null) {
-                System.out.println("The parking lot " + lot + " is already free\n");
+                System.out.println("The parking lot " + lot + " is already free.\n");
             } else {
                 garageLots[lot] = null;
             }
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("The parking lot doesn't exist. Choose another one\n");
+            System.out.println("The parking lot doesn't exist. Choose another one.\n");
         } finally {
             return removedVehicle;
         }
@@ -59,18 +59,23 @@ public class Garage {
     /**
      * method that cycles through the garageLots array to check, with an if else block, if the lots are free printing
      * that the lot is free, or there is a vehicle printing its details.
+     *
+     * lotNum is a temporary variable just for graphic purposes.
      */
     public void printGarageDetails(){
 
         System.out.println("*** GARAGE - Max parking lot: 15 ***\n");
+        int lotNum = 0;
         for (MotorVehicle lot : garageLots) {
             if (lot == null){
-                System.out.println("Free parking lot");
+                System.out.println("Parking lot [" + lotNum + "] free.");
                 System.out.println("****************************************");
+                lotNum++;
             } else {
                 System.out.println("Parking lot [" + Arrays.asList(garageLots).indexOf(lot) + "]: " + lot.getClass().getSimpleName());
                 lot.info();
                 System.out.println("****************************************");
+                lotNum++;
             }
         }
     }
